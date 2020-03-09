d1237466111ec1b832c128b694f3702b94967c66
Tigerminde <tevanyanjan85@mail.ru>
2020-03-09 17:54:07 +0300

    asyncawait

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// TODO: write your code here
+import GameSavingLoader from './gameSavingLoader';
+
+(async () => {
+  try {
+    const gameSavingLoader = await GameSavingLoader.load();
+    console.log(gameSavingLoader);
+  } catch (error) {
+    console.log(error);
+  }
+})();
